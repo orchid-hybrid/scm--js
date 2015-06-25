f33cd8c9ace08861a8e8a2621e8dc670656b3c99
orchid hybrid <orchid.hybrid@gmail.com>
2015-06-25 23:21:30 +0000

    bugfixinbuild

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-csi test.scm -e '(std)' > standard.js
-csi test.scm -e '(run)' > script.js
-csi test.scm -e '(compile-compiler)' > compiler.js
+csi scm-\>js.scm test.scm -e '(std)' > standard.js
+csi scm-\>js.scm test.scm -e '(run)' > script.js
+csi scm-\>js.scm test.scm -e '(compile-compiler)' > compiler.js
