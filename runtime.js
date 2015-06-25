425b7ff6022afcf8ab42dc3ddb37a052f3b68cff
tca <theconartist@gmail.com>
2015-06-24 20:32:25 -0500

    add lambda and variables

--- runtime.js
+++ runtime.js
@@ -1,5 +1,8 @@
 function runtime_dash_booleanize(b){return (b != false);}
 
+
+function car(c){return c.car;}
+function cdr(c){return c.cdr;}
 function js_dash_plus(x,y){return x+y}
 function js_dash_minus(x,y){return x-y}
 function js_dash_times(x,y){return x*y}
