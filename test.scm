66e9495c4bdb87c7fab60e346b7a98d39219789b
tca <theconartist@gmail.com>
2015-06-25 23:32:49 -0500

    compile + eval test

--- test.scm
+++ test.scm
@@ -1,3 +1,65 @@
+(define standard
+  '((define (caar x) (car (car x)))
+    (define (cadr x) (car (cdr x)))
+    (define (cdar x) (cdr (car x)))
+    (define (cddr x) (cdr (cdr x)))
+    (define (caaar x) (car (car (car x))))
+    (define (caadr x) (car (car (cdr x))))
+    (define (cadar x) (car (cdr (car x))))
+    (define (caddr x) (car (cdr (cdr x))))
+    (define (cdraar x) (cdr (car (car x))))
+    (define (cdadr x) (cdr (car (cdr x))))
+    (define (cddar x) (cdr (cdr (car x))))
+    (define (cdddr x) (cdr (cdr (cdr x))))
+    (define (caaaar x) (car (car (car (car x)))))
+    (define (caaadr x) (car (car (car (cdr x)))))
+    (define (caadar x) (car (car (cdr (car x)))))
+    (define (caaddr x) (car (car (cdr (cdr x)))))
+    (define (cadaar x) (car (cdr (car (car x)))))
+    (define (cadadr x) (car (cdr (car (cdr x)))))
+    (define (caddar x) (car (cdr (cdr (car x)))))
+    (define (cadddr x) (car (cdr (cdr (cdr x)))))
+    (define (cdaaar x) (cdr (car (car (car x)))))
+    (define (cdaadr x) (cdr (car (car (cdr x)))))
+    (define (cdadar x) (cdr (car (cdr (car x)))))
+    (define (cdaddr x) (cdr (car (cdr (cdr x)))))
+    (define (cddaar x) (cdr (cdr (car (car x)))))
+    (define (cddadr x) (cdr (cdr (car (cdr x)))))
+    (define (cdddar x) (cdr (cdr (cdr (car x)))))
+    (define (cddddr x) (cdr (cdr (cdr (cdr x)))))
+
+    (define (map f xs)
+      (if (null? xs) '() (cons (f (car xs)) (map f (cdr xs)))))
+
+    (define (for-each f xs)
+      (if (null? xs) '() (begin (f (car xs)) (map f (cdr xs)))))
+
+    (define (append x y)
+      (if (null? x) y (cons (car x) (append (cdr x) y))))
+
+    (define (not b)
+      (if b #f #t))
+    (define (list? l) (or (null? l) (pair? l)))
+    (define (char->string c) c) ;; js
+    (define (list->string l)
+      (if (null? l)
+	  ""
+	  (string-append (char->string (car l)) (list->string (cdr l)))))
+
+    (define (length l) (if (null? l) 0 (+ 1 (length (cdr l)))))
+
+    (define (go t) (display "(") (js->javascript (scm->js t)) (display ")") (newline))
+
+    (define (go1 t) (with-output-to-string (lambda () (go t))))
+
+    (define (go-top t)
+      (for-each (lambda (t) (js->javascript (scm-top->js t)) (display ";") (newline)) t)
+      (newline))
+
+    (define (go-top1 t) (with-output-to-string (lambda () (go-top t))))
+
+    ))
+
 
 (define t1 '42)
 (define t2 '(car (cons 4 2)))
@@ -52,16 +114,22 @@
 (define t41 '(even? 41))
 (define t42 '(even? 42))
 
-(define m1 `(go ',t1))
-(define m2 `(write (scm->js ',t2)))
-(define m3 `(write (scm->js ',t3)))
-(define m4 `(js->javascript '(js-funcall* (js-var car) (js-funcall* (js-var cons) 4))))
+(define m1 `(go1 ',t1))
+(define m2 `(go1 ',t2))
+(define m3 `(go1 ',t3))
+(define m4 `(go1 ',t4))
 (define m5 `(go1 ',t5))
+
+(define p1
+  `(eval (go-top1
+          '((define (for-each1 f xs)
+              (if (null? xs) '() (begin (f (car xs)) (map f (cdr xs)))))
+            (with-output-to-string (lambda () (for-each1 display (string->list "foobar"))))))))
 
 (define (go t) (display "(") (js->javascript (scm->js t)) (display ")") (newline))
 
 (define tests (list t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11 t12 t13 t14 t15 t16 t17 t18 t19 t20 t21 t22 t23 t24 t25 t26 t27 t28 t29 t30 t31 t32 t33 t34 t35 t36 t37 t38 t39 t40 t41 t42
-		    m1 m2 m3 m4 m5))
+		    m1 m2 m3 m4 m5 p1))
 
 (define (run)
   (display (with-output-to-string
@@ -82,62 +150,6 @@
 (define (go-top t)
   (for-each (lambda (t) (js->javascript (scm-top->js t)) (display ";") (newline)) t)
   (newline))
-
-(define standard
-  '((define (caar x) (car (car x)))
-    (define (cadr x) (car (cdr x)))
-    (define (cdar x) (cdr (car x)))
-    (define (cddr x) (cdr (cdr x)))
-    (define (caaar x) (car (car (car x))))
-    (define (caadr x) (car (car (cdr x))))
-    (define (cadar x) (car (cdr (car x))))
-    (define (caddr x) (car (cdr (cdr x))))
-    (define (cdraar x) (cdr (car (car x))))
-    (define (cdadr x) (cdr (car (cdr x))))
-    (define (cddar x) (cdr (cdr (car x))))
-    (define (cdddr x) (cdr (cdr (cdr x))))
-    (define (caaaar x) (car (car (car (car x)))))
-    (define (caaadr x) (car (car (car (cdr x)))))
-    (define (caadar x) (car (car (cdr (car x)))))
-    (define (caaddr x) (car (car (cdr (cdr x)))))
-    (define (cadaar x) (car (cdr (car (car x)))))
-    (define (cadadr x) (car (cdr (car (cdr x)))))
-    (define (caddar x) (car (cdr (cdr (car x)))))
-    (define (cadddr x) (car (cdr (cdr (cdr x)))))
-    (define (cdaaar x) (cdr (car (car (car x)))))
-    (define (cdaadr x) (cdr (car (car (cdr x)))))
-    (define (cdadar x) (cdr (car (cdr (car x)))))
-    (define (cdaddr x) (cdr (car (cdr (cdr x)))))
-    (define (cddaar x) (cdr (cdr (car (car x)))))
-    (define (cddadr x) (cdr (cdr (car (cdr x)))))
-    (define (cdddar x) (cdr (cdr (cdr (car x)))))
-    (define (cddddr x) (cdr (cdr (cdr (cdr x)))))
-
-    (define (map f xs)
-      (if (null? xs) '() (cons (f (car xs)) (map f (cdr xs)))))
-
-    (define (append x y)
-      (if (null? x) y (cons (car x) (append (cdr x) y))))
-
-    (define (not b)
-      (if b #f #t))
-    (define (list? l) (or (null? l) (pair? l)))
-    (define (char->string c) c) ;; js
-    (define (list->string l)
-      (if (null? l)
-	  ""
-	  (string-append (char->string (car l)) (list->string (cdr l)))))
-
-    (define (length l) (if (null? l) 0 (+ 1 (length (cdr l)))))
-    
-    
-    (define (go t) (display "(") (js->javascript (scm->js t)) (display ")") (newline))
-    
-    (define (go1 t) (with-output-to-string (lambda () (go t))))
-    
-    
-    
-    ))
 
 (define (std)
   (go-top standard))
